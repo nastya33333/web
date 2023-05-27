8074f94cadaca3eee73ccf85886bc51f5c27b437
Анастасия Кропачева <kropachevaav15@gmail.com>
2023-05-27 10:30:18 +0000

    Upload New File

---
+++ JS4.11/src/App2.js
@@ -0,0 +1,15 @@
+import "./styles.css";
+
+export default function App2() {
+  return (
+    <div className="App2">
+      <h1>Museums and art centres</h1>
+      <h2>Museums and art centres in Great Britain</h2>
+      <p>In the 18th and 19th centuries the British aristocracy and rich merchants filled their houses with valuable paintings, sculptures, furniture and ornaments which they brought back from their travels abroad. So their collections can be seen today in museums, country houses, palaces and castles. There are museums and art galleries in most cities as well.</p>
+      <p>The national museums and art galleries in London contain some of the most comprehensive collections of objects of artistic, archaeological, scientific, historical and general interest. They are the British Museum, the Victoria and Albert Museum, the Science Museum, the National Gallery, the Tate Gallery, the National Portrait Gallery, the Geological Museum, the Natural History Museum. There are national museums and art galleries in Scotland, Wales, and Northern Ireland. Some of them are the National Museum of Wales in Cardiff, the Ulster Museum in Belfast. The oldest museum the world was founded in 1683 in Oxford.</p>
+      <p>There is much to see in Britain, especially in its heart, London. The British Museum is the largest in the world. It was built between 1823 and 1852. Its magnificent library has the right, by law, to one copy of every publication printed in Britain. Things to single out include the Rosetta Stone in the Southern Egyptian Gallery, and, in the manuscript room, the Magna Charta, Nelson's log book and Scott's last diary. The Victoria and Albert Museum displays fine and applied arts of all countries and periods. Of special interest are the costume displays, the jewellery and porcelain, belonging to the Crown, and the best collection of English miniatures to be found in the country.</p>
+      <p>The National Gallery is the largest in the country. It exhibits all schools of European paintings from the 13th to the 19th century. The Tate Gallery is really three galleries: a national gallery of British art, a gallery of modern sculpture and a gallery of modern foreign paintings. Among the treasures to be found are modern sculpture by Rodin, Moore and Epstein.</p>
+      <p>The Science museum houses the national collections of science, industry and medicine. Many exhibits are full size and there are many historic objects of scientific and technological significance. Additionally there are exhibits sectioned to show their internal construction, and working models. Most cities and towns have museums devoted to arts, archaeology and natural history, usually administered by the local authorities or by individuals. Many private art collections are open to the public. An increasing number of open air museums depict the regional life of an area. About 2,000 museums and art galleries in Britain attract over 68 million people a year.</p>
+    </div>
+  );
+}
