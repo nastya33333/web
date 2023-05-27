f2cbaa33d08944c19ecfd3d910a8f86d2a434c53
Анастасия Кропачева <kropachevaav15@gmail.com>
2023-05-27 10:29:57 +0000

    Upload New File

---
+++ JS4.11/src/App.js
@@ -0,0 +1,15 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Winston Churchill</h1>
+      <h2>British famous people: Winston Churchill</h2>
+      <p>The Right Honourable Sir Winston Leonard Spencer-Churchill, KG. ОМ. CH. FRS (November 30, 1874 — January 24, 1965) was a British politician, best known as Prime Minister of the United Kingdom during World War II. At various times an author, soldier, journalist, legislator and painter, Churchill is generally regarded as one of the most important leaders in British and world history.</p>
+      <p>Winston Churchill was born at Blenheim Palace, near Woodstock in Oxfordshire. Winston's father, Lord Randolph Churchill, was a politician. Winston's mother, Lady Randolph Churchill of Brooklyn, New York, was a daughter of American millionaire Leonard Jerome. As the son of a prominent politician, it was unsurprising that Churchill was soon drawn into politics himself.</p>
+      <p>He started speaking at a number of Conservative meetings in the 1890s. In the 1906 general election, Churchill won a seat in Manchester. He served as Under Secretary of State for the Colonies. Churchill soon became the most prominent member of the Government. At the outbreak of the Second World War Churchill was appointed First Lord of the Admiralty. He was an early supporter of the pan-Europeanism that led to the formation of the European Common market and later the European Union (for which one of the three main buildings of the European Parliament is named in his honour).</p>
+      <p>Miscellany — In 1953 he was awarded two major honours. He was knighted and became Sir Winston Churchill and he was awarded the Nobel Prize for Literature "for his mastery of historical and biographical description as well as for brilliant oratory in defending exalted human values. He was named Time Magazine "Man of the Halt-Century" in the early 1950s. In 1959 Churchill inherited the title of Father of the House. He became the MP with the longest continuous service — since 1924.</p>
+      <p>Churchill College, a constituent college of the University of Cambridge, was founded in 1960 as the national and commonwealth memorial to Winston Churchill. Churchill was voted as "The Greatest Briton" in 2002 "100 Greatest Britons" poll sponsored by the BBC and voted for by the public.</p>
+    </div>
+  );
+}
